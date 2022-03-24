2ef1a90f73088e167679098be058818de7546433
Louis Lingatong <louis.li@digitalroominc.com>
2022-03-24 12:50:28 +0800

    Apply demo updates

--- src/app/services/workService.js
+++ src/app/services/workService.js
@@ -72,7 +72,7 @@ export function exportWorkHistory(id) {
 
 export function downloadFile(path) {
   return new Promise((resolve, reject) => {
-    Http.get(process.env.REACT_APP_API_URL.replace('v1', 'storage') + path, {responseType: 'blob'})
+    Http.get( 'works/file-download', {params: {path}, responseType: 'blob'})
       .then(res => {
         resolve(res.data);
       })
